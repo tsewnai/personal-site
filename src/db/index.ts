92b6e44145360d9e9096da5c62e0a787814c8051
tsewnai <tsewnai@gmail.com>
2026-02-15 17:01:07 -0500

    good ol' imports

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql";
 
 export function getDb(env: { TURSO_DATABASE_URL: string; TURSO_AUTH_TOKEN: string }) {
